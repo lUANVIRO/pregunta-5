155f0ef1d61e65733cebb22129e707617b9c7289
lUANVIRO <l.a.r.v@outlook.es>
2021-12-13 14:43:38 -0600

    first commit

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-var contenedor = document.createElement("div")
-var div = document.getElementById('principal')
-div.appendChild(contenedor)
-
-axios.get('https://mocki.io/v1/d4867d8b-b5d5-4a48-a4ab-79131b5809b8')
-.then(function (res) {
-    let datos = res.data;
-    for(i in datos){
-        contenedor.innerHTML = datos[i].name + " vive en " + datos[i].city + "<br>";
-    }   
-})
-.catch()
-
+function cargar(){
+    axios.get('https://mocki.io/v1/d4867d8b-b5d5-4a48-a4ab-79131b5809b8')
+    .then(function (res) {
+        let datos = res.data;
+        for(var i in datos){
+            if (datos.hasOwnProperty(i)) {
+                var contenedor = document.createElement("div")
+                var div = document.getElementById('principal')
+                div.appendChild(contenedor)
+                contenedor.innerHTML = datos[i].name + " vive en " + datos[i].city + "<br>";
+                console.log(i)
+            }
+        }  
+    })
+    .catch()
+}
