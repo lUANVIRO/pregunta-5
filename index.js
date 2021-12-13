3b339fd61080665d2fd406a0501d4eab47d909d5
lUANVIRO <l.a.r.v@outlook.es>
2021-12-13 14:22:04 -0600

    first commit

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ axios.get('https://mocki.io/v1/d4867d8b-b5d5-4a48-a4ab-79131b5809b8')
     let datos = res.data;
     for(i in datos){
         contenedor.innerHTML = datos[i].name + " vive en " + datos[i].city + "<br>";
-    }
-    
+    }   
 })
 .catch()
 
